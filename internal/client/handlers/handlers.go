3ca14fc89e12ebbb3e92641a1a24653a1fcc6cfd
Vova4o <gavrilenko2012@gmail.com>
2025-02-11 23:06:48 +0700

    fix(ci.yml): lintern secret

--- internal/client/handlers/handlers.go
+++ internal/client/handlers/handlers.go
@@ -368,8 +368,6 @@ func (c *GRPCClient) DeleteDataFromServer(ctx context.Context, dbID int) error {
 func (c *GRPCClient) UpdateDataOnServer(ctx context.Context, data models.Data) error {
 	c.log.Info("Update data handle called.")
 
-	
-
 	encryptedData, err := encryptData(data, c.MasterPassword)
 	if err != nil {
 		c.log.Error("Failed to encrypt data: " + err.Error())
